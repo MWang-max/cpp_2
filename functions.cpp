3b990b4407fdfbebab7176d51da0b91283874e02
MWang-max <mwang2769@gmail.com>
2026-09-08 11:52:04 -0400

    Update functions.cpp

--- functions.cpp
+++ functions.cpp
@@ -1,21 +1,30 @@
 #include <iostream>
 
-void say_hello(std::string user_name, int user_age)
+using namespace std; // avoid "using namespace" unless for std or other very common name spaces to avoid collisions
+
+void say_hello(string user_name, int user_age)
 {
-    std::cout << "Hello, world" << std::endl;
-    std::cout << "Name: " << user_name << std::endl;
-    std::cout << "Age: " << user_age << std::endl;
+    cout << "Hello, world" << endl;
+    cout << "Name: " << user_name << endl;
+    cout << "Age: " << user_age << endl;
     return;
 }
 
-int triple_number(int number) // return statement
-{
-    return number * 3;
+namespace abc {
+    int triple_number(int number) // return statement
+    {
+        return number * 3;
+    }
+
+    void cout() {}
 }
+
+int a;
 
 void print_triple_number(int number) // call function inside another function (not recursion)
 {
-    std::cout << triple_number(number) << std::endl;
+    a = 3; // a can be modified here (in scope)
+    cout << abc::triple_number(number) << endl; // must include abc for triple_number as it is in namespace
 }
 
 int main()
@@ -23,13 +32,28 @@ int main()
     say_hello("John Doe", 30); // variables must be in same order as in declaration
     say_hello("Jane Doe", 40);
 
-    std::cout << triple_number(10) << std::endl;
-    std::cout << triple_number(-2) << std::endl;
-    std::cout << triple_number(0) << std::endl;
+    cout << abc::triple_number(10) << endl;
+    cout << abc::triple_number(-2) << endl;
+    cout << abc::triple_number(0) << endl;
 
+    abc::cout(); // different from std::cout
 
     print_triple_number(4);
     print_triple_number(-3);
+
+    a = 2;
+    int b;
+    b = 4;
+
+    int number; // 'number' variable can be used as different variables with the same name with different scopes (different functions), this variable has a different scope from the other two
+
+    {
+        a = 4; // a and b also in scope to be modified here
+        b = 5;
+        int c;
+    }
     
+    // c = 3; c not in scope (more nested), cannot be modified here
+
     return 0;
 }
